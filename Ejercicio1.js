db4e814acddbf989350ef4a49169438380783828
Cresnik1702 <cresnik17021983@gmail.com>
2021-04-06 11:24:10 +0200

    Ejercicio terminado

---
+++ Ejercicio1.js
@@ -0,0 +1,20 @@
+// Ejercicio - 01 pedir al usuario que nos de dos 
+// números y que sean mostrados por pantalla.
+
+// En pseudo codigo el ejercicio hera de la siguiente manera:
+
+//  Proceso Ejercicio_01
+	
+//	  Definir a, b Como Entero;
+//  	Escribir "Dame dos números";
+//  	Leer a,b;
+//  	Escribir "Tus números son: ", a, " y ", b;
+	
+//  FinProceso
+
+
+var num1 = prompt (" Dame el primer número");
+var num2 = prompt (" Dame el segundo número");
+
+console.log(num1);
+console.log(num2);
